a114029b924fb1666073bccbbbf9c7970f023d3e
Kestutis101 <miceikakestutis@gmail.com>
2023-05-19 23:37:19 +0300

    add functionality to app, auth, logout, validation

--- frontend/beauty-salon/src/App.js
+++ frontend/beauty-salon/src/App.js
@@ -1,19 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/UI/organisms/Login/Login";
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/UI/atoms/Login/Login";
 import Main from "./components/UI/organisms/Main/Main";
-import CreateClient from "./components/UI/organisms/CreateClient/CreateClient";
-import HandleBadRequest from "./components/UI/atoms/HandleBadRequest/HandleBadRequest";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/clients' element={<Main />} />
-        <Route path='/createClient' element={<CreateClient />} />
-        <Route path='/*' element={<HandleBadRequest />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path='/' element={<Main />} />
+      {!isLoggedIn ? (
+        <Route
+          path='*'
+          element={<Login onLogin={() => setIsLoggedIn(true)} />}
+        />
+      ) : (
+        <Navigate to='/' replace />
+      )}
+    </Routes>
   );
 }
 
