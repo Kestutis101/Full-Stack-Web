0f5379058a6901bf3b10f80ad55444aaa0e49b84
Kestutis101 <miceikakestutis@gmail.com>
2023-05-20 13:59:46 +0300

    fixed Routes

--- frontend/beauty-salon/src/App.js
+++ frontend/beauty-salon/src/App.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/UI/atoms/Login/Login";
 import Main from "./components/UI/organisms/Main/Main";
+import Clients from "./components/UI/organisms/Clients/Clients";
+import CreateClient from "./components/UI/organisms/CreateClient/CreateClient";
+import HandleBadRequest from "./components/UI/atoms/HandleBadRequest/HandleBadRequest";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  function handleLogged() {
+    if (localStorage.getItem("jwtToken")) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }
+
   return (
     <Routes>
       <Route path='/' element={<Main />} />
-      {!isLoggedIn ? (
-        <Route
-          path='*'
-          element={<Login onLogin={() => setIsLoggedIn(true)} />}
-        />
-      ) : (
-        <Navigate to='/' replace />
-      )}
+      <Route path='/login' element={<Login handleLogged={handleLogged} />} />
+      <Route
+        path='/clients'
+        element={isLoggedIn ? <Clients /> : <Navigate to='/' replace />}
+      />
+      <Route
+        path='/createClient'
+        element={isLoggedIn ? <CreateClient /> : <Navigate to='/' replace />}
+      />
+      <Route path='/*' element={<HandleBadRequest />} />
     </Routes>
   );
 }
