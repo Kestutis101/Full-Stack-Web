9c0fbfee08e1c94394d97d09fb5fe41ad403b2e7
Kestutis101 <miceikakestutis@gmail.com>
2023-05-20 22:22:12 +0300

    some optimizations

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -35,21 +35,19 @@ export async function register(req, res) {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({
-      email,
-      password: hashedPassword,
-    });
+    const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: "Registration successful" });
+    res.status(201).json({ message: "Registration successfully" });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
